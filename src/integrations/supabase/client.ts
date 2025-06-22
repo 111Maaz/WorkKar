0b610d7ada9ffbd5e55869a7d0dbbca7900546bc
Mohammed Maaz Ali <maazali53093@gmail.com>
2025-06-22 17:48:24 +0530

    database email confirms errors solved

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://kxyvrigahplujprkvqay.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4eXZyaWdhaHBsdWpwcmt2cWF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MTc3MDAsImV4cCI6MjA2NTk5MzcwMH0.V---5bOLUyA6W34JCczBgSNfeOZ2bZ0o0pwnicqXgOc";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
